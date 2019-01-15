150384d26f8c5f4592945ceb3f26fcf200aceb26
Maxwell Huenink <maxh76@users.noreply.github.com>
2019-01-15 17:36:59 -0600

    Made discord bot! TP works and is better than python!

---
+++ discordBot/MyCommands.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.API;
+using System.Threading.Tasks;
+
+namespace discordBot
+{
+    public class RequireBotMod : PreconditionAttribute
+    {
+        protected readonly static ulong[] BotMods = new ulong[] { 259532984909168659, 212687824816701441 };
+        protected bool IsBotMod(IUser user) => BotMods.Contains(user.Id);
+
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services) =>
+            Task.Run(() => IsBotMod(context.User) ?
+                PreconditionResult.FromSuccess() :
+                PreconditionResult.FromError($"{context.User.Username} is not a bot mod"));
+    }
+
+    public abstract class Commands : ModuleBase
+    {
+        protected readonly static ulong[] BotMods = new ulong[] { 259532984909168659, 212687824816701441 };
+        protected bool IsBotMod(IUser user) => BotMods.Contains(user.Id);
+    }
+
+    public class TextCommands : Commands
+    {
+        [Command("tp")]
+        public async Task TP([Remainder]string content)
+        {
+            if (content == null)
+                return;
+
+            var channelID = Context.Message.MentionedChannelIds;
+            var roleID = Context.Message.MentionedRoleIds;
+            var memberID = Context.Message.MentionedUserIds;
+
+            string[] splitContent = content.Split(new char[] { ' ' });
+            string lastArg = splitContent.LastOrDefault();
+            char lastArgType = lastArg.FirstOrDefault(a => a == '#' || a == '@');
+
+            IGuildUser destUser = null;
+            IVoiceChannel destChannel = null;
+            
+            ulong lastArgID = 0;
+            if (!ulong.TryParse(lastArg.Remove(lastArg.Length - 1).Remove(0, 2), out lastArgID))
+            {
+                if (!ulong.TryParse(lastArg, out lastArgID))
+                {
+                    await ReplyAsync("Couldn't find the id.");
+                    return;
+                }
+            }
+
+            if (string.IsNullOrEmpty(lastArgType.ToString())) // assume just id
+            {
+                await ReplyAsync("Can't work with just an id");
+                return;
+            }
+            else if (lastArgType == '@') // member
+            {
+                destUser = await Context.Guild.GetUserAsync(lastArgID);
+            }
+            else if (lastArgType == '#') // channel
+            {
+                destChannel = await Context.Guild.GetVoiceChannelAsync(lastArgID);
+            }
+
+            if (destChannel == null)
+            {
+                if (destUser == null)
+                {
+                    await ReplyAsync("No voice channel or user found");
+                    return;
+                }
+                if (destUser.VoiceChannel == null)
+                {
+                    await ReplyAsync("User is not in voice");
+                    return;
+                }
+
+                destChannel = destUser.VoiceChannel;
+            }
+
+            foreach (ulong id in memberID)
+            {
+                IGuildUser user = await Context.Guild.GetUserAsync(id);
+                await user.ModifyAsync(a => a.Channel = new Optional<IVoiceChannel>(destChannel));
+            }
+            if (IsBotMod(Context.Message.Author))
+            {
+                var users = await Context.Guild.GetUsersAsync();
+                if (splitContent.Length == 1)
+                {
+                    foreach (IGuildUser user in users)
+                    {
+                        if (string.IsNullOrEmpty(user.VoiceSessionId))
+                            continue;
+                        await user.ModifyAsync(b => b.Channel = new Optional<IVoiceChannel>(destChannel));
+                    }
+                    return;
+                }
+                if (roleID.Count >= 0)
+                {
+                    foreach (ulong id in roleID)
+                    {
+                        foreach (IGuildUser user in users)
+                        {
+                            if (string.IsNullOrEmpty(user.VoiceSessionId))
+                                continue;
+                            if (user.RoleIds.Any(a => a == id))
+                                await user.ModifyAsync(b => b.Channel = new Optional<IVoiceChannel>(destChannel));
+                        }
+                    }
+                }
+                return;
+            }
+            await ReplyAsync("You are not a bot mod.");
+        }
+    }
+
+    [Group("debug")]
+    [RequireBotMod()]
+    public class DebugCommands : Commands
+    {
+        [Command("roles")]
+        public async Task GetRoles()
+        {
+            IGuild g = Context.Guild;
+            if (g == null) { return; }
+            await ReplyAsync(
+                "Roles:\n" +
+                g.Roles.OrderByDescending(p => p.Position).
+                    Aggregate<IRole, string>(string.Empty, (a, b) => (a += $"{b.Position} {b.Name}: {b.Id}\n")));
+        }
+
+        [Command("echo")]
+        public async Task Echo([Remainder]string input)
+        {
+            await ReplyAsync(input);
+        }
+
+        [Command("exit")]
+        public async Task Exit()
+        {
+            await Context.Client.StopAsync();
+            await Exit();
+        }
+    }
+}
--- discordBot/Program.cs
+++ discordBot/Program.cs
@@ -1,12 +1,93 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace discordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private readonly DiscordSocketClient client;
+        private readonly CommandService commands;
+        private readonly IServiceProvider services;
+
+        static void Main(string[] args) => new Program().Start(args).GetAwaiter().GetResult();
+
+        public Program()
         {
-            Console.WriteLine("Hello World!");
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+                MessageCacheSize = 20,
+            });
+            commands = new CommandService();
+            services = new ServiceCollection().BuildServiceProvider();
+        }
+
+        public async Task Start(string[] args)
+        {
+            client.Log += Logger;
+            //client.MessageReceived += MessageReceived;
+
+            await InstallCommands();
+
+            await client.LoginAsync(TokenType.Bot, "MzgzMzA5MDU1Mzk0ODQwNTc3.DPkc0g.ehU_ZfUQpxe9wt83nimB-_d3LX0");
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            client.MessageReceived += HandleCommand;
+
+            client.MessageDeleted += HandleDelete;
+            // Discover all of the commands in this assembly and load them.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasStringPrefix("m!", ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+            // Create a Command Context
+            var context = new CommandContext(client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            var result = await commands.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        public async Task HandleDelete(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel)
+        {
+            IMessage x = await message.GetOrDownloadAsync();
+            await x.Channel.SendMessageAsync($"The delete message was from {x.Author} and was: {x.Content} with {x.Embeds.Count} embeds");
+        }
+
+        private Task Logger(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            Console.WriteLine($"Received message from {message.Author.Username} in {(message.Channel as IGuildChannel).Guild}#{message.Channel.Name}. Message contents:\n\t{message.Content}");
+            if (message.Content.ToLower() == "ping!")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
         }
     }
 }
